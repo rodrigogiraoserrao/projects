363ba896b39be0cd18228586dc7e7f0d3cb16d7d
RGS <rodrigogiraoserrao@gmail.com>
2019-11-11 00:18:26 +0000

    Update shuffled_card_deck.py

--- whatAreTheOdds/shuffled_card_deck.py
+++ whatAreTheOdds/shuffled_card_deck.py
@@ -13,11 +13,12 @@ two_in_a_row = 0    # count how many times the deck has two equal cards in a row
 pair_counter = 0    # count how many of these pairs are found in a shuffled deck
 how_many_pairs = [] # the history of how many pairs per deck for some stats
 three_in_a_row = 0  # count how many times the deck has three equal cards in a row
+four_in_a_row = 0   # count how many times the deck has four equal cards in a row
 
 # estimate the probability that a thoroughly shuffled deck contains any two cards in a row with the same value
 for i in range(trials):
     if 0 == (i % (trials // 100)):
-        print(i / trials)
+        print(f"{100 * i / trials}%\r")
     shuffle(cards)
     for j in range(len(cards) - 1):
         if cards[j] == cards[j+1]:
@@ -27,6 +28,8 @@ for i in range(trials):
                 two_in_a_row += 1
             if j < len(cards) - 2 and cards[j] == cards[j+2]:
                 three_in_a_row += 1
+                if j < len(cards) - 3 and cards[j] == cards[j+3]:
+                    four_in_a_row += 1
     if not pair_counter:
         all_different += 1
     else:
@@ -37,7 +40,27 @@ print(all_different + two_in_a_row)
 print(f"{round(100*all_different/trials, 2)}% of all shuffles had no two equal cards in a row")
 print(f"{round(100*two_in_a_row/trials, 2)}% of all shuffles had two or more cards in a row, once or more times")
 print(f"{round(100*three_in_a_row/trials, 2)}% of all shuffles had three or more cards in a row, once or more times")
+print(f"{round(100*four_in_a_row/trials, 2)}% of all shuffles had four cards in a row, once or more times")
 
 print()
 for i in range(1, 11):
     print(f"{i:02} : {round(100*how_many_pairs.count(i)/trials, 2)}%")
+    
+"""
+Sample output:
+4.55% of all shuffles had no two equal cards in a row
+95.45% of all shuffles had two or more cards in a row, once or more times
+11.77% of all shuffles had three or more cards in a row, once or more times
+0.23% of all shuffles had four cards in a row, once or more times
+
+01 : 14.47%
+02 : 22.54%
+03 : 23.13%
+04 : 17.3%
+05 : 10.21%
+06 : 4.9%
+07 : 1.95%
+08 : 0.68%
+09 : 0.21%
+10 : 0.05%
+"""
