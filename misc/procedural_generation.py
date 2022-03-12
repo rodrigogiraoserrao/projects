b6a73f93dda452378bc42e12b720e0fc8a858475
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-03-12 10:10:51 +0000

    Attempt at more efficient generation.

--- misc/procedural_generation.py
+++ misc/procedural_generation.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 import random
 import sys
 
@@ -18,6 +19,14 @@ def random_colour():
     return [random.randint(0, 255) for _ in range(3)]
 
 
+def dist(p1, p2):
+    return math.sqrt(dist_sq(p1, p2))
+
+
+def dist_sq(p1, p2):
+    return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+
+
 if __name__ == "__main__":
     points = [
         (random.randint(0, WIDTH), random.randint(0, HEIGHT))
@@ -25,14 +34,31 @@ if __name__ == "__main__":
     ]
     colours = [random_colour() for _ in range(POINTS)]
 
-    for x, y in itertools.product(range(WIDTH), range(HEIGHT)):
+    mapped = set()
+    dists = []
+    for p1 in points:
+        p1_dists = [dist_sq(p1, p2) for p2 in points if p1 != p2]
+        dists.append((p1, int(math.sqrt(min(p1_dists)) // 2)))
+
+    # Paint the circles that are close enough to each point.
+    for (centre, d), colour in zip(dists, colours):
+        for dx, dy in itertools.product(range(-d, d + 1), repeat=2):
+            p = (centre[0] + dx, centre[1] + dy)
+            if dist(p, centre) <= d:
+                mapped.add(p)
+                screen.set_at(p, colour)
+        pygame.display.flip()
+
+    for xy in itertools.product(range(WIDTH), range(HEIGHT)):
+        if xy in mapped:  # Skip points already painted.
+            continue
         min_dist, min_colour = float("+inf"), (0, 0, 0)
         for point, colour in zip(points, colours):
-            dist = (point[0] - x) ** 2 + (point[1] - y) ** 2
-            if dist < min_dist:
-                min_dist = dist
+            d = dist_sq(point, xy)
+            if d < min_dist:
+                min_dist = d
                 min_colour = colour
-        screen.set_at((x, y), min_colour)
+        screen.set_at(xy, min_colour)
 
     pygame.display.flip()
 
